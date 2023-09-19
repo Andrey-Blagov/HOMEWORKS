6590ecd86309aebce4b89126d4a3666592bb2a61
Andrey <blagovandrey1323@gmail.com>
2023-09-19 06:32:54 +0300

    add Seminars

---
+++ Seminars/Sem1/Program.cs
@@ -0,0 +1,38 @@
+﻿//System.Console.WriteLine("Input number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine($"Your number -> {num} ");
+
+//System.Console.Write("Input number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine($"Quad of {num} -> {num*num}");
+
+//System.Console.Write("Input three-digit number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if(num >= 100 && num < 1000)
+//{
+    //System.Console.WriteLine($"Last digit of {num} -> {num%10}");
+//}
+//else
+//{
+    //System.Console.WriteLine("You iput not three-digit number");
+//}
+
+//System.Console.Write("Input number: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int count = -n;
+//while (count <= n)
+//{
+//System.Console.Write(count++);
+//}
+
+Console.WriteLine("Input first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a == b*b)
+{
+    Console.WriteLine($"{a} is not quad {b}");
+}
+
+   
