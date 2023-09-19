655068fdd2c4202e96974db9779ed53d8fb04a3c
Andrey <blagovandrey1323@gmail.com>
2023-09-19 13:01:58 +0300

    Complited Tasks2

--- HomeWork1/Task2/Program.cs
+++ HomeWork1/Task2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите Первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = b;
+if (b > a) max = b;
+if (b > a) min = a;
+System.Console.WriteLine($"Max= {max}, Min= {min}");
