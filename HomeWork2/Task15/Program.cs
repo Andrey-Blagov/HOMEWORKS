66c8dae269cef3c9e865d6c6977a83edbdbcb137
Andrey <blagovandrey1323@gmail.com>
2023-09-23 18:09:10 +0300

    add Task15

--- HomeWork2/Task15/Program.cs
+++ HomeWork2/Task15/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void DayOfTheWeek(int num)
+{
+    if(num > 5)
+    {
+        System.Console.WriteLine($"{num} - Сегодня Выходной!!!");
+    }
+    else
+    {
+        System.Console.WriteLine($"{num} - На Работу(((");
+    }
+}
+int num = new Random().Next(1,8);
+DayOfTheWeek(num);
