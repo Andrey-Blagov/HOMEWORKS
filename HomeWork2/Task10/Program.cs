b0c22becb7180a1d06b63b2d1e993b118e84deae
Andrey <blagovandrey1323@gmail.com>
2023-09-23 12:11:38 +0300

    add HomeWork2

--- HomeWork2/Task10/Program.cs
+++ HomeWork2/Task10/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void digitals(int num)
+{
+    int hund = num / 100;
+    int dec = (num / 10) % 10;
+    int ed = num % 10;
+    if(hund == 0 && dec == 0)
+    {
+      Console.WriteLine($"Число {num} - Число ОДНОЗНАЧНОЕ !");
+    }  
+    else
+    {
+     if(hund > 0)
+     {
+         Console.WriteLine($"{num} -> Второй знак {dec}, Третий знак {hund}");
+     }
+     else
+     {
+         Console.WriteLine($"{num} -> Второй знак {dec}, Третий знак ОТСУТСТВУЕТ");
+     } 
+    }
+}
+int num = new Random().Next(0,10);
+digitals(num);
