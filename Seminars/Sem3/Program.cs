ad18c5f8de5b33816c68563931f10522cfeeaca0
Andrey <blagovandrey1323@gmail.com>
2023-09-25 14:48:53 +0300

    add sem3_1

--- Seminars/Sem3/Program.cs
+++ Seminars/Sem3/Program.cs
@@ -2,11 +2,11 @@
 
 //int FindQuareter(int x, int y)
 //{
-  //  if(x>0 && y>0) return 1;
-   // if(x<0 && y>0) return 2;
-   // if(x<0 && y<0) return 3;
-   // if(x>0 && y<0) return 4;
-  //  return 0;
+//  //  if(x>0 && y>0) return 1;
+//   // if(x<0 && y>0) return 2;
+// if(x<0 && y<0) return 3;
+// if(x>0 && y<0) return 4;
+//  return 0;
 //}
 //Console.WriteLine("Input X: ");
 //int a = Convert.ToInt32(Console.ReadLine());
@@ -19,14 +19,14 @@
 
 //void QuadTable(int n)
 //{
-  //  int current = 1;
- //   Console.Write(n + " -> ");
-  //  while(current < n)
- //   {
- //      System.Console.Write(current * current + ", ");
-  //     current++;
-  //  }
-  //  Console.WriteLine(current * current);
+//  int current = 1;
+//   Console.Write(n + " -> ");
+//  while(current < n)
+//   {
+//      System.Console.Write(current * current + ", ");
+//     current++;
+//  }
+//  Console.WriteLine(current * current);
 //}
 //Console.Write("Input N: ");
 //int n = Convert.ToInt32(Console.ReadLine());
@@ -39,9 +39,9 @@
 //A (3,6); B (2,1) -> 5,09
 //A (7,-5); B (1,-1) -> 7,21
 
-double Distance (int double xa, double ya, double xb, double yb)
-{
-    return Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2))
-}
-System.Console.WriteLine("Напиши значения координат: ");
-System.Console.WriteLine();
+//double Distance (int double xa, double ya, double xb, double yb)
+//{
+// //   return Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2))
+//}
+//System.Console.WriteLine("Напиши значения координат: ");
+//System.Console.WriteLine();
