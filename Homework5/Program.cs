dd929118d497d3da5c615927993131bc18f54f9b
Andrey <blagovandrey1323@gmail.com>
2023-10-06 15:02:47 +0300

    add H5

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿/* void ArrayDigital (string[] args)
+{
+    Console.Write("Введите количество чисел: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    
+    int count = 0;
+    for(int i = 0; i < n; i++)
+    {
+      Console.Write("Введите число №" + (i+1) + ": ");
+      int num = Convert.ToInt32(Console.ReadLine());
+      
+      if(num > 0)
+        count++;
+    }
+    
+    Console.WriteLine("Количество чисел больше 0: " + count);
+}
+ArrayDigital(args); */
+
+System.Console.Write("Input b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
