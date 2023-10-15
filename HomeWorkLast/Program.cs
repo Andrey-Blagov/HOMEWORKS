68fbd2e29cdb212d2f02ef24bf779170b415e21e
Andrey <blagovandrey1323@gmail.com>
2023-10-15 16:50:55 +0300

    Add HWL

--- HomeWorkLast/Program.cs
+++ HomeWorkLast/Program.cs
@@ -7,14 +7,20 @@
 
 int[] array = { 1, 4, 8, 100, 4, 9};
 int max = array [0];
-int temp = array[0];
+//int temp = array[0];
 for (int i = 0; i < array.Length; i++)
 {  
    if (array[i] > max) max = array[i];
-   array[0] = temp;
-   max = array[0];
+   //array[0] = temp;
+   //max = array[0];
    //temp = 
 }
-
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+}
 Console.WriteLine($"{max}");
-System.Console.WriteLine($"{array}");
+PrintArray(array);
