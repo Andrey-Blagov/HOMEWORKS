e3d4993e52792263783e06515f187636e503e30e
Andrey <blagovandrey1323@gmail.com>
2023-10-16 12:50:19 +0300

    Add S9

--- HomeWorkLast/Program.cs
+++ HomeWorkLast/Program.cs
@@ -48,3 +48,18 @@ for (int i = 0; i < n; i++)
     Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
 }
 
+
+
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+      {
+           for(int j = 0; j < matrix.GetLength(1) + 1; j++)
+           {
+                if(matrix[i, j] > matrix[i, j - 1])
+                {
+                     int temp = matrix[i, j];
+                     matrix[i, j] = matrix[i, j - 1];
+                     matrix[i, j - 1] = temp;
+                 }
+           }
+      }
